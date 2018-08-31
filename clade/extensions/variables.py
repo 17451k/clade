54bf03d8f6ce186846dc8db53f1d17afaaaf6e6f
Ilja Zakharov <ilja.zakharov@ispras.ru>
2018-08-31 09:48:27 +0300

    Fix a minor typo in todo

--- clade/extensions/variables.py
+++ clade/extensions/variables.py
@@ -54,7 +54,7 @@ class Variables(Callgraph):
         self.dump_data_by_key(self.variables, self.variables_suffix)
         self.dump_data(self.used_in_vars, self.used_in_vars_file)
 
-    # todo: Remove this as probkem with ujson dump will be solved
+    # todo: Remove this as problem with ujson dump will be solved
     def dump_data(self, data, file_name):
         """Dump data to a json file in the object working directory."""
 
