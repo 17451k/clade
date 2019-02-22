107969013e9be3614d9f988e6c6eb54323481d53
Ilya Shchepetkov <shchepetkov@ispras.ru>
2019-02-22 16:46:17 +0300

    Mark current version as v2.2.dev1

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ except ImportError:
 
 setuptools.setup(
     name="clade",
-    version="2.1",
+    version="2.2.dev1",
     author="Ilya Shchepetkov",
     author_email="ilya.shchepetkov@yandex.ru",
     url="https://github.com/17451k/clade",
