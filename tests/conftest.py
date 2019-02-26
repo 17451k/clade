61f015ee1b683abf701bd324d0e84e7c32324c40
Ilya Shchepetkov <shchepetkov@ispras.ru>
2019-02-26 13:36:03 +0300

    Do not use multiprocessing with tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,9 @@ from tests.test_intercept import test_project_make
 
 @pytest.fixture(scope="session")
 def cmds_file():
+    # Disable multiprocessing
+    os.environ["CLADE_DEBUG"] = "1"
+
     test_cmds_file = os.path.join(os.path.dirname(__file__), "test_project", "cmds.txt")
 
     if not os.path.isfile(test_cmds_file):
