49c056b3a2be4b643b4603de926a463273aac5de
Ilya Shchepetkov <shchepetkov@ispras.ru>
2019-07-31 13:12:08 +0300

    Refactoring and optimizations in CL

--- clade/extensions/cl.py
+++ clade/extensions/cl.py
@@ -36,6 +36,25 @@ class CL(Compiler):
 
     def parse_cmd(self, cmd):
         self.debug("Parse: {}".format(cmd))
+        parsed_cmd = self.__parse_opts(cmd)
+
+        if self.is_bad(parsed_cmd):
+            self.dump_bad_cmd_by_id(cmd["id"], parsed_cmd)
+            return
+
+        deps = set(self.__get_deps(cmd["id"], parsed_cmd) + parsed_cmd["in"])
+        self.debug("Dependencies: {}".format(deps))
+        self.dump_deps_by_id(cmd["id"], deps)
+
+        self.debug("Parsed command: {}".format(parsed_cmd))
+        self.dump_cmd_by_id(cmd["id"], parsed_cmd)
+
+        if self.conf.get(
+            "Compiler.store_deps"
+        ) and self.is_a_compilation_command(parsed_cmd):
+            self.store_deps_files(deps, parsed_cmd["cwd"])
+
+    def __parse_opts(self, cmd):
         parsed_cmd = self._get_cmd_dict(cmd)
 
         if self.name not in requires_value:
@@ -128,21 +147,7 @@ class CL(Compiler):
                     )
                     parsed_cmd["out"].append(os.path.join(cmd["cwd"], i_name))
 
-        if self.is_bad(parsed_cmd):
-            self.dump_bad_cmd_by_id(cmd["id"], parsed_cmd)
-            return
-
-        deps = set(self.__get_deps(cmd["id"], parsed_cmd) + parsed_cmd["in"])
-        self.debug("Dependencies: {}".format(deps))
-        self.dump_deps_by_id(cmd["id"], deps)
-
-        self.debug("Parsed command: {}".format(parsed_cmd))
-        self.dump_cmd_by_id(cmd["id"], parsed_cmd)
-
-        if self.conf.get(
-            "Compiler.store_deps"
-        ) and self.is_a_compilation_command(parsed_cmd):
-            self.store_deps_files(deps, parsed_cmd["cwd"])
+        return parsed_cmd
 
     def __get_deps(self, cmd_id, cmd):
         """Get a list of CL command dependencies."""
@@ -156,10 +161,12 @@ class CL(Compiler):
     def __collect_deps(self, cmd_id, cmd, cmd_in):
         deps_file = os.path.join(self.temp_dir, "{}-deps.txt".format(cmd_id))
 
+        opts = ["/showIncludes", "/P"] if self.conf.get("Compiler.preprocess_cmds") else ["/showIncludes"]
+
         deps_cmd = (
             [cmd["command"][0]]
             + cmd["opts"]
-            + ["/showIncludes", "/P"]
+            + opts
             + [cmd_in]
             + self.conf.get("Compiler.extra_preprocessor_opts", [])
         )
@@ -174,6 +181,7 @@ class CL(Compiler):
             )
             proc.communicate()
 
+        if self.conf.get("Compiler.preprocess_cmds"):
             if not proc.returncode:
                 self.__preprocess_cmd(cmd, cmd_in)
             else:
