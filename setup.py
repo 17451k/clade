7970534d75e29a7643466414297cb5d0684ac3fe
Ilya Shchepetkov <shchepetkov@ispras.ru>
2022-02-27 11:52:12 +0400

    Change version to v3.4.11

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ except ImportError:
 
 setuptools.setup(
     name="clade",
-    version="3.4.10",
+    version="3.4.11",
     author="Ilya Shchepetkov",
     author_email="shchepetkov@ispras.ru",
     url="https://github.com/17451k/clade",
