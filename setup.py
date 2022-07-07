d930f1d01cf3c1c7ac73c88d08750727fc5e0926
Ilya Shchepetkov <ilya.shchepetkov@gmail.com>
2022-07-07 16:33:36 +0300

    Bump version to v3.6

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ except ImportError:
 
 setuptools.setup(
     name="clade",
-    version="3.5.1",
+    version="3.6",
     author="Ilya Shchepetkov",
     author_email="shchepetkov@ispras.ru",
     url="https://github.com/17451k/clade",
