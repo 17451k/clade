bb64f5954eee3ab7ee7dae2aeab048d49f599e47
Ilya Shchepetkov <ilya.shchepetkov@gmail.com>
2022-11-10 16:43:34 +0400

    Add LN extension

---
+++ clade/extensions/ln.py
@@ -0,0 +1,56 @@
+# Copyright (c) Ilya Shchepetkov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import re
+
+from clade.extensions.common import Common
+
+
+class LN(Common):
+    __version__ = "1"
+
+    def parse(self, cmds_file):
+        super().parse(cmds_file, self.conf.get("LN.which_list", []))
+
+    def parse_cmd(self, cmd):
+        parsed_cmd = {
+            "id": cmd["id"],
+            "in": [],
+            "out": [],
+            "opts": [],
+            "cwd": cmd["cwd"],
+            "command": cmd["command"],
+        }
+
+        # 'ln' options always have the following form:
+        #     [-opt]... in_file out_file (or out_dir)
+        for opt in cmd["command"][1:]:
+            if re.search(r"^-", opt):
+                parsed_cmd["opts"].append(opt)
+            elif not parsed_cmd["in"]:
+                parsed_cmd["in"].append(os.path.normpath(opt))
+            else:
+                out = os.path.normpath(opt)
+
+                if os.path.isdir(out):
+                    out = os.path.join(out, os.path.basename(parsed_cmd["in"][0]))
+
+                parsed_cmd["out"].append(out)
+
+        if self.is_bad(parsed_cmd):
+            self.dump_bad_cmd_id(cmd["id"])
+            return
+
+        self.dump_cmd_by_id(cmd["id"], parsed_cmd)
