8f172a8fad8ce38a2389c1e4e734fe31a570605d
Ilya Shchepetkov <ilya.shchepetkov@gmail.com>
2022-12-13 19:52:30 +0300

    Return base_print preset

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ except ImportError:
 
 setuptools.setup(
     name="clade",
-    version="4.0.1",
+    version="4.0.2",
     author="Ilya Shchepetkov",
     author_email="shchepetkov@ispras.ru",
     url="https://github.com/17451k/clade",
